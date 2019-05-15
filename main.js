9b389994b85948d0aba167572a172ccaa0ac82d9
Mallory Woods <malvwoods@gmail.com>
2019-05-15 17:17:00 -0400

    completed simple liker app

--- main.js
+++ main.js
@@ -1,10 +1,50 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+// error div at top of document
+const ERROR_MODAL = document.getElementById("modal")
 
-// Your JavaScript code goes here!
 
+function makeFull(target){
+  target.innerText = FULL_HEART
+  target.className = ('activated-heart')
+}
 
+function throwError(error){
+    ERROR_MODAL.className = ('');
+    ERROR_MODAL.innerText = error
+    setTimeout(function() {
+      ERROR_MODAL.className = ('hidden')
+    }, 5000)
+}
+
+// setTimeout(throwError, 5000)
+
+function makeFetch(event) {
+  mimicServerCall()
+  .then(function() {
+    makeFull(event.target)})
+  .catch(response => {
+    console.log("ERROR ERROR")
+    throwError(response)
+  })
+}
+
+const heartClick = function(event) {
+  if(event.target.innerText === EMPTY_HEART) {
+    makeFetch(event);
+  } else {
+    event.target.innerText = EMPTY_HEART
+    event.target.className = ('')
+  }
+}
+
+const heartGlyphs = document.querySelectorAll(".like-glyph")
+const allHearts = Array.from(heartGlyphs)
+
+allHearts.forEach(function (heart) {
+  heart.addEventListener("click", heartClick)
+})
 
 
 //------------------------------------------------------------------------------
